4fa4b20da6e6cce7313fcdca026fccf8a782a0dd
VIGNESH <vigneshmskt@gmail.com>
2022-04-06 02:32:15 +0530

    Update wsserver.js

--- wsserver.js
+++ wsserver.js
@@ -7,8 +7,11 @@ var server = http.createServer( app );
 const sio = require("socket.io")(server, {
     handlePreflightRequest: (req, res) => {
         const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "https://master.d2dktscasgyu0l.amplifyapp.com/",
+		"Access-Control-Allow-Methods":"OPTIONS, GET",
+		"Access-Control-Request-Method":"*",
+
             "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
