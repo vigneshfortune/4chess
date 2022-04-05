46346e9f611646fbae67bdd652b2cb2a2fc40dd2
VIGNESH <vigneshmskt@gmail.com>
2022-04-06 02:19:46 +0530

    Cors pollicy

--- wsserver.js
+++ wsserver.js
@@ -1,14 +1,20 @@
 var e    = require('express');
 
-const { Server } = require('socket.io'); 
-
-// Construct a socket
-const sio = new Server(server);
 var http = require('http');
 const PORT = process.env.PORT || 3000;
 var app = e();
 var server = http.createServer( app );
-
+const sio = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 var io  = sio.listen(server);
 
 var player = 0;
